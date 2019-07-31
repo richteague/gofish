c9cf88af0c11f72245fb774d947d46550b743f7b
richteague <richarddteague@gmail.com>
2019-07-31 16:55:40 -0400

    stuff to get pip installable

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+setuptools.setup(
+    name="gofish",
+    version="0.1.2",
+    author="Richard Teague",
+    author_email="rteague@umich.edu",
+    description="Fishing for molecular line emission in protoplanetary disks.",
+    url="https://github.com/richteague/gofish",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "numpy",
+        "astropy",
+        "scipy"
+      ]
+)
